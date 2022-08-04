555960dccc1a180628db5115f765ebe843ea287a
101Amine <aminebecha8@gmail.com>
2022-08-04 15:46:41 +0100

    navbar completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { TopNavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar/>
+    </div>
+  );
+}
+
+export default App;
